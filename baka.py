3b3fcb5ca5b0578cda2aadef9514c89ad1673148
Tobias Willemsen <toobswillemsen@gmail.com>
2024-11-21 20:36:23 +0100

    Delete baka.py

--- baka.py
+++
@@ -1,7 +0,0 @@
-print('he jua wat heb jij grote zwarte gaten hihihi sterrenkunde reference')
-class Jua:
-    def __init__(self,poepen,lopen,schijten,pissen): 
-        pass
-for x in range(4):
-
-    
